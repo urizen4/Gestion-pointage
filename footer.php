67e25edd438c3f6aca814c37e916c0f85494e2c4
BAMBA.AIDARA <caidaraisidk@groupeisi.com>
2021-11-14 02:01:55 +0000

    BOP

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\materialize.js"></script>
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\script.js"></script>
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\canvasjs.min.js"></script>
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\canvasjs.react.js"></script>
-<script type="text/javascript" src="\GestionUtilisateur\public\JavaScript\chart.js"></script>
+<script type="text/javascript" src="/public/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="/public/js/materialize.min.js"></script>
+<script type="text/javascript" src="/public/js/script.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="\GestionUtilisateur\public\Css\materialize.min.css">
-	<link rel="stylesheet" type="text/css" href="\GestionUtilisateur\public\Css\materialize.css">
-	<link rel="stylesheet" type="text/css" href="\GestionUtilisateur\public\Css\style.css">
+	<link rel="stylesheet" type="text/css" href="/public/Css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="/public/Css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="/public/Css/style.css">
 </head>
 <body>
 
